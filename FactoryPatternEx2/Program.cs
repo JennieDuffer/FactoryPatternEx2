95bdf42a5f45a0ad1b2e3f6ff824365ba7f75740
Jennie Duffer <jenniej297@gmail.com>
2022-08-25 14:05:25 -0500

    initial commit

---
+++ FactoryPatternEx2/Program.cs
@@ -0,0 +1,14 @@
+﻿using FactoryPatternEx2;
+
+Console.WriteLine("What database do you want to use:    ");
+Console.WriteLine();
+Console.WriteLine("List");
+Console.WriteLine("SQL");
+Console.WriteLine("Mongo");
+Console.WriteLine();
+
+var response = Console.ReadLine();
+
+IDataAccess database = DataAccessFactory.GetDataAccessType(response);
+database.LoadData();
+database.SaveData();
---
+++ FactoryPatternEx2/SQLDataAccess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternEx2
+{
+    public class SQLDataAccess : IDataAccess
+    {
+        public void LoadData()
+        {
+            Console.WriteLine("I am reading data from SQLDataAccess");
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine("I am saving data to a SQLDataAccess");
+        }
+    }
+}
